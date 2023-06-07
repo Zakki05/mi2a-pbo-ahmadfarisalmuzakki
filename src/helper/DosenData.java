155c18deb45a2884191ad455295bc1d89989bf55
Zakki05 <ahmadfaris771@gmail.com>
2023-06-07 23:00:48 +0700

    Tugas Form Dosen Pertemuan (07/06/2023)

---
+++ src/helper/DosenData.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helper;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class DosenData {
+    public static final String [][] data = {
+        {"2101092028","Ahmad Faris Almuzakki","MI","L"},
+        {"2101092022","Aatrisa Mulyati","TEKOM","P"},
+        {"2101092020","Fadel Firmansyah","MI","L"},
+        {"2101092026","Yamaa","TRPL","L"},
+    };
+    
+    public static final String [] TABLE_HEADER=
+    {"Nobp","Nama","Prodi","Gender"};
+}
---
+++ src/helper/DummyData.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helper;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class DummyData {
+    public static final String [][] data = {
+        {"2101092028","Ahmad Faris Almuzakki","L"},
+        {"2101092034","Abi Lutfiandy Hendri","L"},
+        {"2101092036","Al Fajri Huda","L"},
+        {"2101092065","Ratih Yudistiviana","P"},
+    };
+    
+    public static final String [] TABLE_HEADER=
+    {"Nobp","Nama","Gender"};
+}
