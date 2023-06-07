76c58a9d6cc119f322abcba0888543e64ebde9d1
Zakki05 <ahmadfaris771@gmail.com>
2023-06-07 23:00:55 +0700

    Tugas Form Dosen Pertemuan (07/06/2023)

---
+++ src/model/MahasiswaModel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import helper.DummyData;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class MahasiswaModel extends DefaultTableModel{
+    public MahasiswaModel(){
+        super(DummyData.data, DummyData.TABLE_HEADER);
+    }
+}
