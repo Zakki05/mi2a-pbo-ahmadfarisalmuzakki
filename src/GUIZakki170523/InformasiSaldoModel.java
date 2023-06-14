68026a9c872ac0a3982a9418663b8e1ab611db05
Zakki05 <ahmadfaris771@gmail.com>
2023-06-14 10:53:27 +0700

    Tugas GUI ATM Bank Pertemuan (17/05/2023)

---
+++ src/GUIZakki170523/InformasiSaldoModel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUIZakki170523;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class InformasiSaldoModel {
+    int saldo=1500000000;
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+    
+}
---
+++ src/GUIZakki170523/PinModel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUIZakki170523;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class PinModel {
+    String pin="123456";
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+      
+}
